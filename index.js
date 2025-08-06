4efa155227c7c815e53c4537951674a3e2bb6a33
Turbash Negi <negirawatdeepi@gmail.com>
2025-08-06 15:52:12 +0530

    Added two auth routes two post routes one for register one for login quite solid now i will make docs also right now not at the end

--- index.js
+++ index.js
@@ -1,12 +1,63 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const dbConnect=require('./db/db');
+const userModel = require('./models/user');
 
+app.use(express.json());
 dotenv.config();
 
 app.get('/', async (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.post('/register', async (req, res) => {
+    const { username, email, password } = req.body;
+    try {
+        let user = await userModel.findOne({ email });
+        if(user){
+            return res.send('User already exists');
+        }
+        bcrypt.genSalt(10,(err,salt)=>{
+            bcrypt.hash(password,salt,async(err,hash)=>{
+                let user=userModel.create({username, email, password: hash});
+                let token=jwt.sign({email,username},process.env.JWT_SECRET);
+                res.send({message: "Registration Successful",token});
+            })
+        })
+    } catch (error) {
+        console.error('Error registering user:', error);
+        res.send('Internal Server Error');
+    }
+});
+
+app.post('/login', async (req,res)=>{
+    const {email,password} = req.body;
+    try{
+        let user = await userModel.findOne({ email });
+        if(!user){
+            return res.send('User not found');
+        }
+        bcrypt.compare(password,user.password,(err,result)=>{
+            if(result){
+                let token = jwt.sign({ email, username: user.username }, process.env.JWT_SECRET);
+                res.send({ message: "Login Successful", token });
+            }
+            else{
+                res.send("Invalid Credentials");
+            }
+        })
+    }
+    catch (error) {
+        console.error('Error logging in user:', error);
+        res.send('Internal Server Error');
+    }
+});
+
+app.listen(3000, async () => {
     try{
         await dbConnect();
         console.log('Database connected successfully');
@@ -14,9 +65,5 @@ app.get('/', async (req, res) => {
     catch(err){
         console.error('Database connection failed:', err);
     }
-    res.send('Hello, World!');
-});
-
-app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
