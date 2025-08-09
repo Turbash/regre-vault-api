46e55ccf3ba56e20eade256c117c37510b0cb682
Turbash Negi <negirawatdeepi@gmail.com>
2025-08-09 17:18:58 +0530

    Added docs for the two routes i created using swagger api also added docs to appear in /docs route

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const app = express();
 const dotenv = require('dotenv');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 
 const dbConnect=require('./db/db');
 const userModel = require('./models/user');
+const swaggerDocument = YAML.load('./openapi.yaml');
 
 app.use(express.json());
 dotenv.config();
@@ -14,12 +17,14 @@ app.get('/', async (req, res) => {
     res.send('Hello, World!');
 });
 
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.post('/register', async (req, res) => {
     const { username, email, password } = req.body;
     try {
         let user = await userModel.findOne({ email });
         if(user){
-            return res.send('User already exists');
+            return res.status(400).send('User already exists');
         }
         bcrypt.genSalt(10,(err,salt)=>{
             bcrypt.hash(password,salt,async(err,hash)=>{
@@ -30,7 +35,7 @@ app.post('/register', async (req, res) => {
         })
     } catch (error) {
         console.error('Error registering user:', error);
-        res.send('Internal Server Error');
+        res.status(500).send('Internal Server Error');
     }
 });
 
